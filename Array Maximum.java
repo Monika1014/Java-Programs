31d9a568757ef844006fde90c7c3186120f8b246
Monika1014 <monikasr1014@gmail.com>
2025-07-23 12:31:20 +0530

    Create Array Maximum.java

---
+++ Array Maximum.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.print(Arrays.toString(arr));
+        System.out.println();
+        int max=arr[0];
+        for(int i=1;i<n;i++)
+        {
+            if(arr[i]>max){
+                System.out.println("The maximum number is : "+arr[i]);
+            }
+            
+        }
+        
+    }
+}
